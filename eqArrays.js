182ca0dbd0aabceef62387aedd7eda30e0e16c97
RoneyThomas <roneythomas6@gmail.com>
2021-11-23 04:07:49 -0500

    Implemented eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,21 @@
+const eqArrays = (array1, array2) => {
+  if (array1.length != array1.length) {
+    return false;
+  }
+  for (let x = 0; x < array1.length; x++) {
+    if (array1[x] !== array2[x]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual(eqArrays([1, 2], [1, 2]), true); // => should PASS
