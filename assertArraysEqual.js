9608109cbce0b71664c07d487b220b7908593e38
RoneyThomas <roneythomas6@gmail.com>
2021-11-23 04:27:58 -0500

    Done with assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,21 @@
+const eqArrays = (array1, array2) => {
+  if (array1.length != array1.length) {
+    return false;
+  }
+  for (let x = 0; x < array1.length; x++) {
+    if (array1[x] !== array2[x]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} === ${array2}`);
+  }
+};
+
+assertArraysEqual([1, 2], [1, 2]); // => should PASS
