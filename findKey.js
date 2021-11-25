dedcc8e3772641132236f1e06c399de54f2d02ef
RoneyThomas <roneythomas6@gmail.com>
2021-11-25 13:33:40 -0500

    Done findKey.js

---
+++ findKey.js
@@ -0,0 +1,24 @@
+const findKey = (data, callback) => {
+  for (const [d, v] of Object.entries(data)) {
+    if (callback(v)) {
+      return d;
+    }
+  }
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2), "noma");
